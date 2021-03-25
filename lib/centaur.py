d7f441d333b776bdd9ec5b8f0bcdb782bed53278
Joshua Carey <jdcarey128@gmail.com>
2021-03-25 14:57:10 -0600

    Redo centaur class

--- lib/centaur.py
+++ lib/centaur.py
@@ -1,35 +1,38 @@
-class Centaur:
+class Centaur: 
     def __init__(self, name, breed):
-        self.name = name
-        self.breed = breed
+        self.name = name 
+        self.breed = breed 
+        self.crankiness_level = 0
         self.standing = True
-        self.cranky_level = 0
 
     def shoot(self):
-        self.cranky_level += 1
-        if self.is_cranky() == False and self.is_laying() == False:
-            return "Twang!!!"
+        self.crankiness_level += 1
+        if self.is_cranky() or self.is_laying():
+            return "NO!" 
         else:
-            return "NO!"
+             return "Twang!!!"
 
     def run(self):
-        self.cranky_level += 1
-        if self.is_cranky() == False and self.is_laying() == False:
-            return "Clop clop clop clop!!!"
+        self.crankiness_level += 1
+        if self.is_cranky() or self.is_laying():
+            return "NO!" 
         else:
-            return "NO!"
+            return "Clop clop clop clop!!!"
+
+    def crankiness_score(self):
+        return 2
 
     def is_cranky(self):
-        return self.cranky_level >= 3
+        return self.crankiness_level > self.crankiness_score()
 
     def is_standing(self):
         return self.standing
 
-    def sleep(self):
-        if self.standing == True:
+    def sleep(self): 
+        if self.standing:
             return "NO!"
-        else:
-            self.cranky_level = 0
+        else: 
+            self.crankiness_level = 0
 
     def lay_down(self):
         self.standing = False
@@ -37,13 +40,13 @@ class Centaur:
     def is_laying(self):
         return not self.standing
 
-    def stand_up(self):
+    def stand_up(self): 
         self.standing = True
 
-    def drink_potion(self):
-        if self.is_standing() == True and self.is_cranky() == False:
-            return "Now I'm sick"
-        elif self.is_standing() == True:
-            self.cranky_level = 0
-        else:
+    def drink_potion(self): 
+        if self.is_laying():
             return "Can't, I'm laying"
+        elif self.crankiness_level == 0:
+            return "Now I'm sick"
+        else: 
+            self.crankiness_level = 0
